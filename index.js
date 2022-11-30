b4436089209423125b690126455884472a3ed33a
AyodejiOyebanji <oyeayodejioyebanji2003@gmail.com>
2022-11-30 09:52:42 +0100

    first commit

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-const express = require("express");
-const app= express();
-const cors =require("cors");
+const express = require('express');
+const app = express();
+const cors = require('cors');
 app.use(cors());
-const bodyParser= require("body-parser");
-app.use(bodyParser.urlencoded({extended:true,limit:"50mb"}));
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 app.use(bodyParser.json());
-const mongoose =require('mongoose');
+const mongoose = require('mongoose');
 require('dotenv').config();
-const URI =process.env.MONGO_URL
-const PORT = process.env.PORT || 7000
+const URI = process.env.MONGO_URL;
+const PORT = process.env.PORT || 7000;
 app.set('*', 'cors');
- const userRouter= require("./routes/user.route")
+const userRouter = require('./routes/user.route');
 
 app.use('/users', userRouter);
-mongoose.connect(URI, (err)=>{
-    if(err){
-        console.log("Not connecting");
-        console.log(err);
-    }else{
-        console.log("Connecting");
-    }
-})
+app.get('/', (_, res) => {
+  res.json({ message: 'Ayo is a good boy' });
+});
+mongoose.connect(URI, (err) => {
+  if (err) {
+    console.log('Not connecting');
+    console.log(err);
+  } else {
+    console.log('Connecting');
+  }
+});
 
-app.listen(PORT, ()=>{
-    console.log(`running on port ${PORT}`);
-})
-module.exports=app
+app.listen(PORT, () => {
+  console.log(`running on port ${PORT}`);
+});
+module.exports = app;
